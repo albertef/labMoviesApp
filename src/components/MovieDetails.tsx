26a469f1dec647fdab82e05d33ad6f740fef9149
albertef <albertef@gmail.com>
2026-05-03 23:32:06 +0100

    Movie Details Page - included production countries.

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -54,6 +54,21 @@ const MovieDetails = (movie: MovieDetailsProps) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip
+            label="Production Countries"
+            sx={styles.chipLabel}
+            color="primary"
+          />
+        </li>
+        {(movie.production_countries ?? []).map((country) => (
+          <li key={country.iso_3166_1}>
+            <Chip label={country.name ?? "Unknown"} />
+          </li>
+        ))}
+      </Paper>
     </>
   );
 };
